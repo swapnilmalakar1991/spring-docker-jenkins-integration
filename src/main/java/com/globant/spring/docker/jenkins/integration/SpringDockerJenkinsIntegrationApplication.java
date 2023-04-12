7cdbf680b2249199a155321c8c2a48cef0655bda
swapnilmalakar1991 <swapnilmalakar.1991@gmail.com>
2023-04-12 18:49:41 +0530

    First Commit

---
+++ src/main/java/com/globant/spring/docker/jenkins/integration/SpringDockerJenkinsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.globant.spring.docker.jenkins.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerJenkinsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerJenkinsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/globant/spring/docker/jenkins/integration/SpringDockerJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.spring.docker.jenkins.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
